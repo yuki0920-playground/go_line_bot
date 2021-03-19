ceb0d470abcea61ff3bc780f3bd237025ceebadc
yuki0920 <ywsep20@gmail.com>
2021-03-19 18:11:21 +0900

    オウム返しBOT追加

---
+++ go.mod
@@ -0,0 +1,8 @@
+module go_line_bot
+
+go 1.16
+
+require (
+	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/line/line-bot-sdk-go v7.8.0+incompatible // indirect
+)
--- main.go
+++ main.go
@@ -4,11 +4,18 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	// LINE SDK
+	"github.com/line/line-bot-sdk-go/linebot"
+	// 環境変数
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	// ハンドラの呼び出し: ルーティングの役割
 	http.HandleFunc("/", helloHandler)
+	http.HandleFunc("/callback", lineHandler)
 
 	// 標準出力
 	fmt.Println("http://localhost:8080 で起動")
@@ -21,4 +28,61 @@ func main() {
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	msg := "Hello World"
 	fmt.Fprint(w, msg)
+}
+
+func lineHandler(w http.ResponseWriter, r *http.Request) {
+	log.Print("リクエスト受信")
+
+	envLoad()
+
+	secretKey := os.Getenv("LINE_SECRET_KEY")
+	secretToken := os.Getenv("LINE_ACCES_TOKEN")
+	fmt.Println("LINE_SECRET_KEY", secretKey)
+	fmt.Println("LINE_ACCES_TOKEN", secretToken)
+
+	// botを初期化
+	bot, err := linebot.New(
+		secretKey,
+		secretToken,
+	)
+
+	// エラーに値があればログに出力する
+	if err != nil {
+		log.Fatal(err)
+	}
+	// botのParseRequestメソッドの引数にlineHandlerを渡す
+	events, err := bot.ParseRequest(r)
+	if err != nil {
+		// エラーがある場合エラーの種類によってレスポンスコードを出し分ける
+		if err == linebot.ErrInvalidSignature {
+			// Bad Request
+			w.WriteHeader(400)
+		} else {
+			// Internal Server Error
+			w.WriteHeader(500)
+		}
+	}
+	// インデックスは不要なので_に入れてループする
+	for _, event := range events {
+		// メッセージタイプのイベントならば
+		if event.Type == linebot.EventTypeMessage {
+			// メッセージを変数に入れてメッセージの種類ごとに判定する
+			switch message := event.Message.(type) {
+			case *linebot.TextMessage:
+				replyMessage := message.Text
+				// 返信用トークンをつけてメッセージを送信する
+				_, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do()
+				if err != nil {
+					log.Print(err)
+				}
+			}
+		}
+	}
+}
+
+func envLoad() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 }
